d666f7038f3cd01b031c03506864d910a48107b9
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-08-15 14:49:42 +0100

    compare getElementById vs geElementsByClass

--- dom.js
+++ dom.js
@@ -3,5 +3,18 @@ console.log(document.getElementsByTagName('li'));
 
 const liCollection = document.getElementsByTagName('li');
 for (const li of liCollection) {
-    console.log(li.innerText);
+    // console.log(li.innerText);
 }
+
+// console.log(document.getElementById('kheda').innerText);
+const title = document.getElementById('kheda');
+title.innerText = 'Kheda Lagse';
+// console.log(title.innerText);
+
+// console.log(document.getElementsByClassName('paragraph'));
+const pTitles = document.getElementsByClassName('paragraph');
+for (let pTitle of pTitles) {
+    pTitle.innerText = 'amra akhon o vat khai ni. aita kono kotha???';
+    console.log(pTitle);
+}
+
